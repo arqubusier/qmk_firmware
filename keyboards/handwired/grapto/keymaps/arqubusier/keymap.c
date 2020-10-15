d77da8e489e00acc6dee3eacea97ff483ba50b65
Herman Lundkvist <herman.lundkvist@gmail.com>
2020-10-15 20:52:12 +0200

    Only row 0 and row 1 working.

--- keyboards/handwired/grapto/keymaps/arqubusier/keymap.c
+++ keyboards/handwired/grapto/keymaps/arqubusier/keymap.c
@@ -31,10 +31,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
                           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,
-                          KC_TRNS, KC_TRNS, KC_TRNS, KC_P,    KC_Y,
-               KC_LSHIFT, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,
+                          KC_1, KC_2, KC_3, KC_P,    KC_Y,
+               KC_0, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,
                           KC_TRNS, KC_Q,    KC_J,    KC_K,    KC_X,
-                                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                   KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,
                KC_ENTER,
 
                           KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,
@@ -81,4 +81,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
     }
     return true;
+}
+
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  debug_matrix=true;
+  //debug_keyboard=true;
+  //debug_mouse=true;
 }
